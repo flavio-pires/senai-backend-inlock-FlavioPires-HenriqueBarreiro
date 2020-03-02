7fa8fa1c0e8a6be82da1906cad380684ba80ec08
Henrique Barreiro Santana <47651807863@info.local>
2020-03-02 11:33:43 -0300

    API

---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Domains/UsuarioDomain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Domains
+{
+    public class UsuarioDomain
+    {
+    }
+}
